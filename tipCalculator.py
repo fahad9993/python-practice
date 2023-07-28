ee661086e803f20b3f36452e166b630555082c48
fahad9993 <fahad9993@gmail.com>
2023-07-28 11:27:48 +0600

    Formatting of results add

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator.")
 
-total_bill = float(input("What was the total bill? "))
+total_bill = float(input("What was the total bill? Taka "))
 
 tip_percentage = float(input("What percentage of tip would you like to give? 10, 12, or 15? "))
 
@@ -10,6 +10,6 @@ total_tip = total_bill * tip_percentage / 100
 
 total_bill_payable = total_bill + total_tip
 
-bill_per_person = round(total_bill_payable / number_of_people, 2)
+bill_per_person = "{:.2f}".format(round(total_bill_payable / number_of_people, 2))
 
-print(f"You have to pay total {total_bill_payable} taka and each person should pay: Taka {bill_per_person}")
+print(f"You have to pay total {total_bill_payable:.2f} taka and each person should pay: Taka {bill_per_person}")
