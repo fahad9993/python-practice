6209191f696124767c29c2c398bf3ec47e02b927
fahad9993 <fahad9993@gmail.com>
2023-07-29 07:17:53 +0600

    Determine category add

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -7,6 +7,22 @@ weight = float(input("Enter your weight in kg: "))
 
 # BMI = round(weight / height ** 2, 0)
 
-BMI = int(weight / height ** 2)
+bmi = int(weight / height ** 2)
 
-print("Your BMI is " + str(BMI))
+# Determining BMI category
+
+category = ""
+
+if bmi <= 18.5:
+    category = "underweight"
+elif bmi <= 25:
+    category = "normal weight"
+elif bmi <= 30:
+    category = "overweight"
+elif bmi <= 35:
+    category = "obese"
+else:
+    category = "clinically obese"
+
+print(f"Your BMI is {bmi} and you are in the \"{category.upper()}\" category.")
+
