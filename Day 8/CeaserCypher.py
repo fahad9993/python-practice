2af043802cb263127fc77300abe557e4583043ce
Fahad <fahad9993@gmail.com>
2023-08-14 11:07:10 +0600

    Remove todos

--- Day 8/CeaserCypher.py
+++ Day 8/CeaserCypher.py
@@ -6,7 +6,6 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def caesar(txt, sft, choice):
     encoded_text = ""
     decoded_text = ""
@@ -31,14 +30,3 @@ def caesar(txt, sft, choice):
 
 
 caesar(txt=text, sft=shift, choice=direction)
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount
-#  and print the encrypted text. e.g. plain_text = "hello" shift = 5 cipher_text = "mjqqt" print output: "The encoded
-#  text is mjqqt"
-
-# #HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-# 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
