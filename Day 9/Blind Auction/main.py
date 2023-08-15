e1dc1734c24307175e52584d9bb9c5b408d90a14
Fahad <fahad9993@gmail.com>
2023-08-15 11:15:26 +0600

    Highest bidder function add

--- Day 9/Blind Auction/main.py
+++ Day 9/Blind Auction/main.py
@@ -5,6 +5,21 @@ bidder_info = []
 
 print("Welcome to the secret auction program.")
 
+
+def highest_bidder(bidder_information):
+    max_bidder_amount = 0
+    max_bidder_name = ""
+
+    for bidder in bidder_information:
+        next_bidder_amount = bidder["amount"]
+        next_bidder_name = bidder["name"]
+        if next_bidder_amount > max_bidder_amount:
+            max_bidder_amount = next_bidder_amount
+            max_bidder_name = next_bidder_name
+
+    print(f"The winner is {max_bidder_name} with a bid of ${max_bidder_amount}.")
+
+
 run_again = True
 while run_again:
     bidder_name = input("What is your name?: ")
@@ -19,23 +34,14 @@ while run_again:
     user_input = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
     if user_input == "no":
         run_again = False
+        highest_bidder(bidder_info)
     else:
         # Clearing the console
         # For this to work you should enable "Emulate terminal in output console"
         # from Run>Edit Configurations>Add new>Python>Modify option>Emulate terminal in output console
         # Then selecting the script path
         import os
+
         os.system('cls')
 
 print(bidder_info)
-
-max_bidder_amount = 0
-max_bidder_name = ""
-for bidder in bidder_info:
-    next_bidder_amount = bidder["amount"]
-    next_bidder_name = bidder["name"]
-    if next_bidder_amount > max_bidder_amount:
-        max_bidder_amount = next_bidder_amount
-        max_bidder_name = next_bidder_name
-
-print(f"The winner is {max_bidder_name} with a bid of ${max_bidder_amount}.")
