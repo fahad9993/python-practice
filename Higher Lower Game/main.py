734b2a4e893ddf4581d415ca4a5b2b7ec374312d
Fahad <fahad9993@gmail.com>
2023-09-05 12:13:40 +0600

    Final Commit

--- Higher Lower Game/main.py
+++ Higher Lower Game/main.py
@@ -2,18 +2,45 @@ from art import logo, vs
 from game_data import data
 import random
 
+# Clearing the console
+# For this to work you should enable "Emulate terminal in output console"
+# from Run>Edit Configurations>Add new>Python>Modify option>Emulate terminal in output console
+# Then selecting the script path
+import os
 
-def compare():
-    return random.sample(range(1, len(data)), k=2)
+score = 0
 
 
-values = compare()
-a_info = data[values[0]]
-b_info = data[values[1]]
-print(a_info, b_info)
+def max_follower(a, b):
+    if a["follower_count"] > b["follower_count"]:
+        return "A"
+    else:
+        return "B"
 
-print(logo)
-print(f"Compare A: {a_info['name']}, a {a_info['description']}, from {a_info['country']}.")
-print(vs)
-print(f"Against B: {b_info['name']}, a {b_info['description']}, from {b_info['country']}.")
-# users_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+run_again = True
+while run_again:
+    os.system('cls')
+    print(logo)
+    if score != 0:
+        print(f"You're right! Current score: {score}")
+    values = random.sample(range(1, len(data)), k=2)
+    a_info = data[values[0]]
+    b_info = data[values[1]]
+    # print(a_info, b_info)
+
+    print(f"Compare A: {a_info['name']}, a {a_info['description']}, from {a_info['country']}.")
+    print(vs)
+    print(f"Against B: {b_info['name']}, a {b_info['description']}, from {b_info['country']}.")
+    users_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    answer = max_follower(a_info, b_info)
+
+    if users_choice == answer:
+        score += 1
+
+    else:
+        os.system('cls')
+        print(logo)
+        print(f"Sorry! That's wrong. Final score: {score}")
+        run_again = False
