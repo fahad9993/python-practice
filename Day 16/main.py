5ec575e7c2b43a0c44725871cc2d9ad354e96a87
Fahad <fahad9993@gmail.com>
2023-10-13 07:37:57 +0600

    Initial commit - using turtle

---
+++ Day 16/main.py
@@ -0,0 +1,20 @@
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+#
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.speed(1)
+# timmy.forward(300)
+# timmy.right(135)
+# timmy.forward(300)
+# timmy.home()
+#
+# my_screen = Screen()
+#
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+import prettytable
+
