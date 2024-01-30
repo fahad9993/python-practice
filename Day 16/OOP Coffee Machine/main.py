0e01c3bc7632d64c34543dca0d4d82baaf8fd484
Fahad <fahad9993@gmail.com>
2024-01-30 10:25:08 +0600

    Day 16 complete

--- Day 16/OOP Coffee Machine/main.py
+++ Day 16/OOP Coffee Machine/main.py
@@ -1,3 +1,13 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+selected_coffee = input("What would you like? (espresso/latte/cappuccino/): ")
+
+print(selected_coffee)
+
+available_items = Menu().get_items()
+print(available_items)
+
+available_ingredients = CoffeeMaker().report()
+print(available_ingredients)
