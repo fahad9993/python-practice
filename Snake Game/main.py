c6594fe223b8b0a9d891def5fad8e6e75477bd59
Fahad <fahad9993@gmail.com>
2024-02-18 07:37:32 +0600

    Generate random food and detect collision with the snake

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,5 +24,8 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
