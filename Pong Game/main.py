f9e7a45344d6a7ae672a9f4b9ee77a535fdec82c
Fahad <fahad9993@gmail.com>
2024-02-19 19:33:13 +0600

    Increase speed everytime ball hits a paddle

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -37,9 +39,11 @@ while is_game_on:
     # Detect if R paddle miss
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect if L paddle miss
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
