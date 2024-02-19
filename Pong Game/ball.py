725a7185af3a7148c5ec2d4094081109be0b45bc
Fahad <fahad9993@gmail.com>
2024-02-19 12:01:24 +0600

    Bounce the ball

--- Pong Game/ball.py
+++ Pong Game/ball.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-import random
 
 
 class Ball(Turtle):
@@ -9,8 +8,13 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
