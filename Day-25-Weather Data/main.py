0e165024db8219fbf9569cda1141d54edf60398e
Fahad <fahad9993@gmail.com>
2024-10-11 11:36:21 +0600

    Use csv module

--- Day-25-Weather Data/main.py
+++ Day-25-Weather Data/main.py
@@ -1,3 +1,16 @@
+# with open("weather-data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+import csv
+
 with open("weather-data.csv") as data_file:
-    data = data_file.readlines()
+    data = csv.reader(data_file)
     print(data)
+    temperature = []
+
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+
+    print(temperature)
