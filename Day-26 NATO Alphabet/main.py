41f25e7891cc2317bc081819d0f8840c268f249e
Fahad <fahad9993@gmail.com>
2024-10-12 18:44:07 +0600

    Final Commit

--- Day-26 NATO Alphabet/main.py
+++ Day-26 NATO Alphabet/main.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(nato_dict)
+# print(nato_dict)
 
 # Create a list of the phonetic code words from a word that the user inputs.
-
+user_input = input("Enter a word: ").upper()
+output = [nato_dict[letter] for letter in user_input]
+print(output)
