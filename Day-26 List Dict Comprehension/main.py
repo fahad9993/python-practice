2eeace2cce4ff59907d57b0e1f0a0ca786735c97
Fahad <fahad9993@gmail.com>
2024-10-12 18:15:36 +0600

    Complete dictionary comprehension-02

--- Day-26 List Dict Comprehension/main.py
+++ Day-26 List Dict Comprehension/main.py
@@ -11,9 +11,31 @@
 
 # Dictionary Comprehension - 01
 
-sentence = "What is the Airspeed Velocity of an unladen Swallow?"
+# sentence = "What is the Airspeed Velocity of an unladen Swallow?"
+#
+# words = sentence.split()
+# result = {word:len(word) for word in words}
+#
+# print(result)
 
-words = sentence.split()
-result = {word:len(word) for word in words}
+# Dictionary Comprehension - 02
 
-print(result)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+
+def to_fahrenheit(c):
+    """Converts temperature in Celsius to Fahrenheit"""
+    return (c * 9 / 5) + 32
+
+
+weather_f = {day: to_fahrenheit(temp_c) for (day, temp_c) in weather_c.items()}
+
+print(weather_f)
