7514419a146c781f0f57cb7f9850ae6735b85023
Fahad <fahad9993@gmail.com>
2024-10-12 17:55:25 +0600

    Complete dictionary comprehension-01

--- Day-26 List Dict Comprehension/main.py
+++ Day-26 List Dict Comprehension/main.py
@@ -1,8 +1,19 @@
-with open("file1.txt") as file_1:
-    data_1 = file_1.readlines()
+# List Comprehension
 
-with open("file2.txt") as file_2:
-    data_2 = file_2.readlines()
+# with open("file1.txt") as file_1:
+#     data_1 = file_1.readlines()
+#
+# with open("file2.txt") as file_2:
+#     data_2 = file_2.readlines()
+#
+# result = [int(num) for num in data_1 if num in data_2]
+# print(result)
 
-result = [int(num) for num in data_1 if num in data_2]
+# Dictionary Comprehension - 01
+
+sentence = "What is the Airspeed Velocity of an unladen Swallow?"
+
+words = sentence.split()
+result = {word:len(word) for word in words}
+
 print(result)
