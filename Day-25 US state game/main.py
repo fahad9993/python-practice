7c8780c3bfe8db17d75fab24a0b1e7ce9425064c
Fahad <fahad9993@gmail.com>
2024-10-12 11:26:10 +0600

    Write state name on the screen

--- Day-25 US state game/main.py
+++ Day-25 US state game/main.py
@@ -1,7 +1,9 @@
+import pandas
 import turtle
-from turtle import Screen
 
-screen = Screen()
+FONT = ("Arial", 10, "normal")
+
+screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -16,7 +18,23 @@ turtle.shape(image)
 #
 # turtle.onscreenclick(get_coordinates)
 
-answer_state = turtle.textinput(title="Guess the state", prompt="What is the another state?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data["state"].tolist()
 
-turtle.mainloop()
+game_is_on = True
+while game_is_on:
+    answer_state = turtle.textinput(title="Guess the State", prompt="What's another state's name?")
+    # Getting the x and y coordinate by matching state
+    answer_state_x = int(data[data.state == answer_state].x.iloc[0])
+    answer_state_y = int(data[data.state == answer_state].y.iloc[0])
+    print(f"x: {answer_state_x}, y: {answer_state_y}")
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(answer_state_x, answer_state_y)
+        t.write(answer_state, align="center", font=FONT)
+
+
+screen.exitonclick()
