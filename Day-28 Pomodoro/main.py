18445ee4b1ef3304a295ffb91d4eb81fb1ac57bc
Fahad <fahad9993@gmail.com>
2024-10-13 22:13:53 +0600

    Add zero in front of seconds less than ten

--- Day-28 Pomodoro/main.py
+++ Day-28 Pomodoro/main.py
@@ -24,6 +24,9 @@ def countdown(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
+    if count_sec < 10:
+        count_sec = "0" + str(count_sec)
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
