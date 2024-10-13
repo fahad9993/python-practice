07fbb474527ae5ef634d0664d05cb7ee501182a0
Fahad <fahad9993@gmail.com>
2024-10-13 12:38:13 +0600

    Use Entry

--- Day-27/main.py
+++ Day-27/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.minsize(width=500, height=300)
+window.title("My first GUI Program")
 
 my_label = Label(text="This is my label", font=("Arial", 10, "bold"))
 my_label.pack()
@@ -9,10 +10,7 @@ my_label.pack()
 
 # Button
 def button_clicked():
-    if my_label["text"] == "This is my label":
-        my_label.config(text="Button got clicked")
-    else:
-        my_label.config(text="This is my label")
+    my_label.config(text=user_input.get())
 
 
 button = Button(text="Click me", command=button_clicked)
@@ -21,7 +19,5 @@ button.pack()
 # Entry
 user_input = Entry(width=10)
 user_input.pack()
-
-
 
 window.mainloop()
