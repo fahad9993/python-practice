6dd57a3e1833e2feee5aaab61ffb8fa468bfa20e
Fahad <fahad9993@gmail.com>
2024-10-13 21:24:22 +0600

    Complete UI

--- Day-28 Pomodoro/main.py
+++ Day-28 Pomodoro/main.py
@@ -20,12 +20,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button_label = Button()
+start_button_label.config(text="Start", highlightthickness=0)
+start_button_label.grid(column=0, row=2)
+
+reset_button_label = Button()
+reset_button_label.config(text="Reset", highlightthickness=0)
+reset_button_label.grid(column=2, row=2)
+
+checkmarks = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmarks.grid(column=1, row=3)
 
 
 window.mainloop()
