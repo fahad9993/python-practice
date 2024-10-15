c630223bc4b3d3d41089bd7241fe34a8f4875467
Fahad <fahad9993@gmail.com>
2024-10-15 19:33:20 +0600

    Handle KeyError

--- Day-26 NATO Alphabet/main.py
+++ Day-26 NATO Alphabet/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-# Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(nato_dict)
+
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-output = [nato_dict[letter] for letter in user_input]
-print(output)
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        output = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
