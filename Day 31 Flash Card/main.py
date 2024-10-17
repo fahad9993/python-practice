fb66b0100ce017b00c6d33ffc25aab0efbf64986
Fahad <fahad9993@gmail.com>
2024-10-17 12:06:57 +0600

    Add functionality to buttons

--- Day 31 Flash Card/main.py
+++ Day 31 Flash Card/main.py
@@ -1,6 +1,20 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 # ------------------- UI SETUP -------------------- #
 
@@ -11,17 +25,17 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Arial", 24, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 36, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 24, "italic"))
+card_word = canvas.create_text(400, 263, text="Word", font=("Arial", 36, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 unknown_button_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=unknown_button_img, highlightthickness=0)
+unknown_button = Button(image=unknown_button_img, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
 known_button_img = PhotoImage(file="images/right.png")
-known_button = Button(image=known_button_img, highlightthickness=0)
+known_button = Button(image=known_button_img, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
 
 window.mainloop()
