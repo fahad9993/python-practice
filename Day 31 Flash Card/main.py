659ac48fb0a383997cf2aaee22e265a8863cd08c
Fahad <fahad9993@gmail.com>
2024-10-17 12:58:50 +0600

    Change bg after flip card

--- Day 31 Flash Card/main.py
+++ Day 31 Flash Card/main.py
@@ -24,9 +24,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 24, "italic"))
-card_word = canvas.create_text(400, 263, text="Word", font=("Arial", 36, "bold"))
+card_back_img = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, font=("Arial", 24, "italic"))
+card_word = canvas.create_text(400, 263, font=("Arial", 36, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
