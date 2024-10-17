dc34e828614bf9e33519df6a6259b870d83f74e8
Fahad <fahad9993@gmail.com>
2024-10-17 12:59:29 +0600

    Add function to flip card

--- Day 31 Flash Card/main.py
+++ Day 31 Flash Card/main.py
@@ -10,10 +10,20 @@ current_card = {}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_bg, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    global current_card
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_bg, image=card_back_img)
 
 
 # ------------------- UI SETUP -------------------- #
