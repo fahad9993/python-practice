180855c9fceb118cef9ca1f9f9b822e9713ad653
Fahad <fahad9993@gmail.com>
2024-10-17 13:04:51 +0600

    Add flip timer

--- Day 31 Flash Card/main.py
+++ Day 31 Flash Card/main.py
@@ -32,6 +32,8 @@ window = Tk()
 window.title("Flushy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
@@ -48,5 +50,7 @@ unknown_button.grid(column=0, row=1)
 known_button_img = PhotoImage(file="images/right.png")
 known_button = Button(image=known_button_img, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
