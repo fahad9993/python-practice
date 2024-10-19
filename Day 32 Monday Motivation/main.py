9f2cb52ba8e47ba6f05a77b1773764243c3dd545
Fahad <fahad9993@gmail.com>
2024-10-20 00:08:39 +0600

    Final Commit

--- Day 32 Monday Motivation/main.py
+++ Day 32 Monday Motivation/main.py
@@ -2,6 +2,8 @@ import datetime as dt
 import smtplib
 import os
 from dotenv import load_dotenv
+import pandas
+import random
 
 load_dotenv()
 
@@ -11,9 +13,20 @@ PASSWORD = os.getenv("PASSWORD")
 TO_EMAIL = os.getenv("TO_EMAIL")
 
 now = dt.datetime.now()
-day_of_week = now.weekday()
-if day_of_week == 5:
-    with smtplib.SMTP(SMTP_SERVER) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_EMAIL, msg="Subject: Hello\n\nThis is the email body.")
+day_today = now.day
+month_today = now.month
+
+for (index, row) in data.iterrows():
+    letter_no = random.randint(1, 3)
+    print(letter_no)
+    if row.month == month_today and row.day == day_today:
+        with open(f"letter_templates/letter_{letter_no}.txt") as letter_file:
+            letter = letter_file.read().replace("[NAME]", row.f_name)
+
+        with smtplib.SMTP(SMTP_SERVER) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=row.email,
+                                msg=f"Subject: Happy Birthday {row.f_name}!\n\n{letter}"
+                                )
