d7a29ce90cd9148aa9af268e5e35ea229699f063
Fahad <fahad9993@gmail.com>
2024-10-23 19:35:34 +0600

    Stop running the app if no question remain

--- Day 34 Quizzler App/ui.py
+++ Day 34 Quizzler App/ui.py
@@ -41,9 +41,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached end of the quiz.")
+            self.right_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
 
     def choose_true(self):
         is_right = self.quiz.check_answer("True")
