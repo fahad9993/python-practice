2c775d43e9b495db02497e247efeea8c1d4b2679
Fahad <fahad9993@gmail.com>
2024-10-23 11:34:54 +0600

    Check if ISS is overhead

--- Day 33 ISS Position/main.py
+++ Day 33 ISS Position/main.py
@@ -9,8 +9,25 @@ parameters = {
     "lng": MY_LONG,
     "time_format": 24,
 }
-response = requests.get(url="https://api.sunrisesunset.io/json", params=parameters)
-response.raise_for_status()
+
+
+def is_iss_overhead():
+    res = requests.get(url="http://api.open-notify.org/iss-now.json")
+    res.raise_for_status()
+
+    iss_data = res.json()
+
+    iss_latitude = float(iss_data["iss_position"]["latitude"])
+    iss_longitude = float(iss_data["iss_position"]["longitude"])
+    print(iss_longitude, iss_latitude)
+
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    response = requests.get(url="https://api.sunrisesunset.io/json", params=parameters)
+    response.raise_for_status()
 
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split(":")[0])
