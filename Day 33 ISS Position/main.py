4c739fed9b4fe1a140d1682871a37c756587e491
Fahad <fahad9993@gmail.com>
2024-10-23 11:34:31 +0600

    Check if it's nighttime

--- Day 33 ISS Position/main.py
+++ Day 33 ISS Position/main.py
@@ -1,15 +1,22 @@
 import requests
-MY_LAT = 23.810331
-MY_LONG = 90.412521
+from datetime import datetime
+
+MY_LAT = 23.810331  # Your latitude
+MY_LONG = 90.412521  # Your longitude
 
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
+    "time_format": 24,
 }
 response = requests.get(url="https://api.sunrisesunset.io/json", params=parameters)
 response.raise_for_status()
 
-data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-print(sunrise, sunset)
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split(":")[0])
+    sunset = int(data["results"]["sunset"].split(":")[0])
+    print(f"sunrise: {sunrise}\nsunset:{sunset}")
+    now = datetime.now().hour
+
+    if sunset <= now or now <= sunrise:
+        return True
