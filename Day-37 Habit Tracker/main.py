83c8c1e54a0cd518e934f22220d4b2aaa7718464
Fahad <fahad9993@gmail.com>
2024-10-24 13:52:07 +0600

    Create a graph

--- Day-37 Habit Tracker/main.py
+++ Day-37 Habit Tracker/main.py
@@ -4,14 +4,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PIXELA_ENDP0INT = "https://pixe.la/v1/users"
+TOKEN = os.getenv("TOKEN")
+USERNAME = os.getenv("USER_NAME")
+
+pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": os.getenv("TOKEN"),
-    "username": os.getenv("USER_NAME"),
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-# response = requests.post(url=PIXELA_ENDP0INT, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+
+graph_params = {
+    "id": "graph1",
+    "name": "Habit Track Graph",
+    "unit": "commit",
+    "type": "int",
+    "color": "momiji",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(response.text)
