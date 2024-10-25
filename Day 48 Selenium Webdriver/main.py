c6008b18e9f091a65f0884fa8443e171f7fa1859
Fahad <fahad9993@gmail.com>
2024-10-25 17:36:11 +0600

    Get events time and name

--- Day 48 Selenium Webdriver/main.py
+++ Day 48 Selenium Webdriver/main.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get(url="https://www.prothom-alo.com")
+driver.get(url="https://www.python.org/")
 
-input("Press enter to close the browser...")
+events_elements = driver.find_elements(By.CSS_SELECTOR, ".event-widget ul li")
+events = [event.find_element(By.TAG_NAME, "a").text for event in events_elements]
+time_list = [event.find_element(By.TAG_NAME, "time").get_attribute(name="datetime").split("T")[0] for
+             event in events_elements]
+
+events_data = {}
+for i in range(len(events)):
+    events_data[i] = {
+        "time": time_list[i],
+        "name": events[i]
+    }
+
+print(events_data)
 driver.quit()
